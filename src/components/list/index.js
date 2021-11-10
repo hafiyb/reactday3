5c6d9a23e2a2626016aa371055d185d91e68cf9d
hafiyb <hafiyb@gmail.com>
2021-11-11 07:35:41 +0800

    attempt to fix image

--- src/components/list/index.js
+++ src/components/list/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import img1 from '../../images/img1.jpg'
+import img2 from '../../images/img2.jpg'
+import img3 from '../../images/img3.jpg'
+import img4 from '../../images/img4.jpg'
+import img5 from '../../images/img5.jpg'
+import img6 from '../../images/img6.jpg'
+import img7 from '../../images/img7.jpg'
+import img8 from '../../images/img8.jpg'
 
 
 
@@ -12,8 +20,8 @@ export default class List extends React.Component{
     render(){
         return(
             
-                <div style={{minHeight:'15vh',width:'100vw',borderBottom:'1px solid black', display:'flex',flexDirection:'row'}}>
-                    <div style={{backgroundColor:'black',height:'100%',width:'200px'}}>
+                <div style={{minHeight:'15vh',width:'100vw',borderBottom:'1px solid black', display:'flex',flexDirection:'row',alignItems:'center'}}>
+                    <div style={{backgroundColor:'black',minHeight:'15vh',width:'200px'}}>
                         <img src={this.props.image} alt="" style={{height:'100%', width:'100%'}} />
                     </div>
                     <Link to={{
