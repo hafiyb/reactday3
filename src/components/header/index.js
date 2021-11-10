b52973795dc13273cc59ecacbbba9822a55703ab
hafiyb <hafiyb@gmail.com>
2021-11-10 16:24:16 +0800

    test

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -7,7 +7,6 @@ export default class Blog extends React.Component{
     render(){
         return(
             <div style={{height:'80px',width:'100vw',backgroundColor:'black',display:'flex',flexDirection:'row',alignItems:'center'}}>
-                {/* <img src='images/shreklogo.png' alt="" /> */}
                 <Link to="/dashboard"><img src={Logo} style={{width:'80px',height:'80px'}} alt="" /></Link>
             </div>
         )
